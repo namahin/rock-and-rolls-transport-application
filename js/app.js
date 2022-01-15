f87ca6e927feaf5dd167ce706ffdc1c4750fda4f
namahin <mahin0w1@gmail.com>
2022-01-16 01:42:48 +0600

    add new css

--- js/app.js
+++ js/app.js
@@ -20,56 +20,36 @@ $('.owl-carousel').owlCarousel({
 })
 
 
-// All Checkbox
-$(function () {
-
-    // Header All Checkbox Event
-    $("#allCheck").on("click", function () {
-        if ($("input:checkbox").prop("checked")) {
-            $("input:checkbox[name='row-check']").prop("checked", true);
-        } else {
-            $("input:checkbox[name='row-check']").prop("checked", false);
-        }
-    });
-
-    // Check event on each table row checkbox
-    $("input:checkbox[name='row-check']").on("change", function () {
-        var total_check_boxes = $("input:checkbox[name='row-check']").length;
-        var total_checked_boxes = $("input:checkbox[name='row-check']:checked").length;
-
-        // If all checked manually then check master checkbox
-        if (total_check_boxes === total_checked_boxes) {
-            $("#allCheck").prop("checked", true);
-        } else {
-            $("#allCheck").prop("checked", false);
-        }
-    });
-});
+// ui-slider more btn
+function openMore() {
+    var element = document.getElementById('dropList');
+    element.classList.toggle('expand');
+}
 
 // OTP input filed
 
 $('.digit-group').find('input').each(function () {
-	$(this).attr('maxlength', 1);
-	$(this).attr('required', 'required');
-	$(this).on('keyup', function (e) {
-		var parent = $($(this).parent());
+    $(this).attr('maxlength', 1);
+    $(this).attr('required', 'required');
+    $(this).on('keyup', function (e) {
+        var parent = $($(this).parent());
 
-		if (e.keyCode === 8 || e.keyCode === 37) {
-			var prev = parent.find('input#' + $(this).data('previous'));
+        if (e.keyCode === 8 || e.keyCode === 37) {
+            var prev = parent.find('input#' + $(this).data('previous'));
 
-			if (prev.length) {
-				$(prev).select();
-			}
-		} else if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode === 39) {
-			var next = parent.find('input#' + $(this).data('next'));
+            if (prev.length) {
+                $(prev).select();
+            }
+        } else if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90) || (e.keyCode >= 96 && e.keyCode <= 105) || e.keyCode === 39) {
+            var next = parent.find('input#' + $(this).data('next'));
 
-			if (next.length) {
-				$(next).select();
-			} else {
-				if (parent.data('autosubmit')) {
-					parent.submit();
-				}
-			}
-		}
-	});
+            if (next.length) {
+                $(next).select();
+            } else {
+                if (parent.data('autosubmit')) {
+                    parent.submit();
+                }
+            }
+        }
+    });
 });
